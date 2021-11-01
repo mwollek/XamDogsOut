4d3248c8a245a9c3d3277c51e9a201423588c162
mwollek <mwollek@student.agh.edu.pl>
2021-11-01 22:39:51 +0100

    Add project files.

---
+++ XamDogsOut/XamDogsOut/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamDogsOut.Models;
+using XamDogsOut.ViewModels;
+using XamDogsOut.Views;
+
+namespace XamDogsOut.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
