eecfe9cef7909927e340953cb9e899adef4c3168
mwollek <mwollek@student.agh.edu.pl>
2021-11-04 21:06:18 +0100

    Added auth MVVM operations

---
+++ XamDogsOut/XamDogsOut/ViewModels/AuthVM.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamDogsOut.Services;
+using XamDogsOut.Views;
+
+namespace XamDogsOut.ViewModels
+{
+
+    public class AuthVM : INotifyPropertyChanged
+    {
+
+        public Command LoginCommand { get; set; }
+        public Command RegisterNavigationCommand { get; set; }
+
+        private string email;
+        public string Email { get { return email; } set { email = value; OnPropertyChanged("EntriesHaveText"); } }
+
+        private string password;
+        public string Password { get { return password; } set { password = value; OnPropertyChanged("EntriesHaveText"); } }
+
+        private bool entriesHaveText;
+        public bool EntriesHaveText
+        {
+            get
+            {
+                bool isEmailEmpty = string.IsNullOrEmpty(Email);
+                bool isPasswordEmpty = string.IsNullOrEmpty(Password);
+
+                return !isEmailEmpty && !isPasswordEmpty;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public AuthVM()
+        {
+            LoginCommand = new Command<bool>(Login, CanLogin);
+            RegisterNavigationCommand = new Command(RegisterNavigation);
+        }
+
+
+        public async void RegisterNavigation()
+        {
+           await App.Current.MainPage.Navigation.PushAsync(new RegisterPage());
+        }
+        public async void Login(bool parameter)
+        {
+            bool result = await Auth.LoginUserAsync(Email, Password);
+            if (result)
+            {
+                await App.Current.MainPage.Navigation.PushAsync(new MapPage());
+            }
+        }
+        private bool CanLogin(bool parameter)
+        {
+            return EntriesHaveText;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
---
+++ XamDogsOut/XamDogsOut/Views/AuthPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamDogsOut.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AuthPage : ContentPage
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+            var assembly = typeof(AuthPage);
+            iconImage.Source = ImageSource.FromResource("XamDogsOut.Assets.Images.paw.png", assembly);
+        }
+    }
+}
