450ec62890b8b32b3fb53b46d7fbb4bacf581a09
mwollek <mwollek@student.agh.edu.pl>
2021-11-25 19:30:20 +0100

    Added Profil model

---
+++ XamDogsOut/XamDogsOut/Models/Profil.cs
@@ -0,0 +1,29 @@
+﻿using Plugin.CloudFirestore.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamDogsOut.Models
+{
+    public class Profil
+    {
+        [Id]
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+
+        // adress
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string BuildingNumber { get; set; }
+        public string FlatNumber { get; set; }
+        public string ZipCode { get; set; }
+
+        //position
+
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+
+    }
+}
